e6c5e435eb96b70af337b42f7521fd63d97b2592
Scott Wilson <sw@nw2s.net>
2023-01-19 09:12:36 -0500

    basic json log format

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,11 +15,22 @@ repositories {
 	mavenCentral()
 }
 
+configurations.all {
+	exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging")
+	exclude(group = "ch.qos.logback", module = "logback-classic")
+	exclude(group = "org.apache.logging.log4j", module = "log4j-to-slf4j")
+}
+
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-log4j2")
+
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("io.github.microutils:kotlin-logging-jvm:3.0.4")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
--- src/main/kotlin/controllers/TestController.kt
+++ src/main/kotlin/controllers/TestController.kt
@@ -3,15 +3,20 @@ package controllers
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import mu.KotlinLogging
 
 
 @RestController
 @RequestMapping("/test")
 class TestController
 {
+    private val logger = KotlinLogging.logger {}
+
     @GetMapping("/isup")
     fun isup() : Boolean
     {
+        logger.info { "Test Request Completed" }
+
         return true
     }
 }
